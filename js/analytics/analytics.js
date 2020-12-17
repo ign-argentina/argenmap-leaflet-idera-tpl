59896204fb16f32f5e81e64e12de5a80790c632b
andreazomoza <aszomoza@gmail.com>
2020-12-17 09:53:31 -0300

    gtag

--- js/analytics/analytics.js
+++ js/analytics/analytics.js
@@ -1,8 +1,15 @@
-//var gaTackingCode = "XX-NNNNNNNNN-N";
-//$.getScript("https://www.googletagmanager.com/gtag/js?id=" + gaTackingCode, function( data, textStatus, jqxhr ) {
-//	window.dataLayer = window.dataLayer || [];			
-//	function gtag(){dataLayer.push(arguments);}
-//    gtag('js', new Date());
-//    gtag('config', gaTackingCode);	
-//});
-	
+// Global site tag (gtag.js) - Google Analytics: GA4 //
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-0T13XW78SK", function( data, textStatus, jqxhr ) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-0T13XW78SK');
+});
+
+// Global site tag (gtag.js) - Google Analytics: Universal //
+$.getScript("https://www.googletagmanager.com/gtag/js?id=UA-136146579-5", function( data, textStatus, jqxhr ) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-136146579-5');
+});
